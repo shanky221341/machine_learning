33b2e4ffe3b141363044ee92cebab5b38fc3a08f
Vineet Berlia <vberlia@in.imshealth.com>
2018-07-04 22:16:24 +0530

    Added method to create featue from frequency counts

---
+++ machineLearning/featureCreation.py
@@ -0,0 +1,22 @@
+class FeatureCreation:
+    '''
+    Outputs result based on the map.
+    '''
+
+    @staticmethod
+    def mapValue(value, condition):
+        for key in condition.keys():
+            k = key.replace('value', str(value))
+            if eval(k):
+                return condition[key]
+    '''
+    This function will take a dataframe(which contains the categorical feature and its count),mapping condition as inputs.
+    Outputs new feature based on the map.
+    Example condition ->
+    d={'value>5':'high_pop','2 <= value <= 5':'mid_pop','value<2':'low_pop'}
+    '''
+
+    @staticmethod
+    def createFeatureFromFrequencyCount(df, condition):
+        new_feature = df['count'].map(lambda value: FeatureCreation.mapValue(value, condition))
+        return new_feature
